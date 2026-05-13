fb71b175b2a556ea27f107634b6f663672fda1d1
007Vd <vdeogade007@gmail.com>
2026-05-13 12:49:52 +0530

    constants and config file completed

---
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+
+#req constants
+
+TARGET_COLUMN="Result"
+PIPELINE_NAME:str="NetworkSecurity"
+ARTIFICAT_DIR:str="Artifacts"
+FILE_NAME:str="phisingData.csv"
+
+TRAIN_FILE_NAME:str="train.csv"
+TEST_FILE_NAME:str="test.csv"
+
+
+DATA_INGESTION_COLLECTION_NAME:str="NetworkData"
+DATA_INGESTION_DATABASE_NAME:str="NS"
+DATA_INGESTION_DIR_NAME:str="data_ingestion"
+DATA_INGESTION_FEATURES_STORE_DIR:str="feature_store"
+DATA_INGESTION_INGESTED_DIR:str="ingested"
+DATA_INGESTED_TRAIN_TEST_SPLIT_RATION:float=0.2
